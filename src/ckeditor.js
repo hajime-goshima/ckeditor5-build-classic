43616cc897a1b775b25e29dc256042adb1a059c6
Hazm <goshima.hajime@gmail.com>
2018-08-09 03:01:08 +0900

    custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,12 +7,15 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+// import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -22,18 +25,23 @@ import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import SimpleUpload from 'ckeditor5-simple-upload/src/simpleupload';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
+	Underline,
+	Strikethrough,
+	Code,
 	BlockQuote,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -42,7 +50,11 @@ ClassicEditor.builtinPlugins = [
 	ImageUpload,
 	Link,
 	List,
-	Paragraph
+	Paragraph,
+	Table,
+	TableToolbar,
+	Alignment,
+	SimpleUpload
 ];
 
 // Editor configuration.
@@ -53,13 +65,18 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'underline',
+			'strikethrough',
+			'code',
 			'link',
+			'alignment',
 			'bulletedList',
 			'numberedList',
 			'imageUpload',
 			'blockQuote',
 			'undo',
-			'redo'
+			'redo',
+			'insertTable'
 		]
 	},
 	image: {
@@ -69,6 +86,23 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'imageTextAlternative'
 		]
+	},
+	heading: {
+		options: [
+			{ model: 'paragraph', title: '段落', class: 'ck-heading_paragraph' },
+			{ model: 'heading1', view: 'h1', title: '見出し 1', class: 'ck-heading_heading1' },
+			{ model: 'heading2', view: 'h2', title: '見出し 2', class: 'ck-heading_heading2' },
+			{ model: 'heading3', view: 'h3', title: '見出し 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: '見出し 4', class: 'ck-heading_heading4' },
+			{ model: 'heading5', view: 'h5', title: '見出し 5', class: 'ck-heading_heading5' },
+			{ model: 'heading6', view: 'h6', title: '見出し 6', class: 'ck-heading_heading6' }
+		]
+	},
+	table: {
+		toolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+	},
+	alignment: {
+		options: [ 'left', 'center', 'right' ]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
